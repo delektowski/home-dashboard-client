8760fbbc8ff8c0da7b2033c53939c2f8659fed18
Marcin Delektowski <marcin.delektowski@gmail.com>
2025-01-25 22:17:11 +0100

    refactor: change config

--- src/app/graphql.provider.ts
+++ src/app/graphql.provider.ts
@@ -1,5 +1,5 @@
-import { provideApollo } from 'apollo-angular';
-import { inject } from '@angular/core';
+import { Apollo, provideApollo } from 'apollo-angular';
+import { inject, Provider } from '@angular/core';
 import { HttpLink } from 'apollo-angular/http';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
@@ -7,7 +7,7 @@ import { InMemoryCache, split } from '@apollo/client/core';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { Kind, OperationTypeNode } from 'graphql/index';
 
-export const graphqlProvider = provideApollo(() => {
+export const graphqlProvider: Provider[] = [provideApollo(() => {
   const httpLink = inject(HttpLink);// Create an http link:
   const http = httpLink.create({
     uri: 'http://localhost:3000/graphql',
@@ -35,4 +35,4 @@ export const graphqlProvider = provideApollo(() => {
     link,
     cache: new InMemoryCache(),
   };
-});
+}), Apollo];
