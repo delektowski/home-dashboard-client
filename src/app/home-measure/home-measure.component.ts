165f5fbd40158ed30c1844533ceecc6e70486766
Marcin Delektowski <marcin.delektowski@gmail.com>
2025-01-26 22:52:06 +0100

    feat: add subscription

--- src/app/home-measure/home-measure.component.ts
+++ src/app/home-measure/home-measure.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
-import { AsyncPipe, NgForOf } from '@angular/common';
+import { NgForOf } from '@angular/common';
 import { map } from 'rxjs';
 
 
@@ -15,11 +15,21 @@ const GET_MEASURES_HOME = gql`
   }
 `;
 
+const MEASURES_HOME_SUBSCRIPTION = gql`
+  subscription {
+    measuresHomeAdded {
+      id
+      placeName
+      temperature
+    }
+  }
+`;
+
 @Component({
   selector: 'app-home-measure',
-  imports: [AsyncPipe, NgForOf],
+  imports: [NgForOf],
   templateUrl: './home-measure.component.html',
-  styleUrl: './home-measure.component.scss'
+  styleUrl: './home-measure.component.scss',
 })
 export class HomeMeasureComponent implements OnInit {
   loading: boolean | undefined;
@@ -30,13 +40,28 @@ export class HomeMeasureComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.homeMeasures = this.apollo
+
+
+    this.apollo
       .watchQuery<any>({
         query: GET_MEASURES_HOME,
         variables: {
           placeName: 'Living Room',
         },
       })
-      .valueChanges.pipe(map(result => result.data.getMeasuresHome));
+      .valueChanges.pipe(map(result => result.data.getMeasuresHome)).subscribe((result) => {
+      this.homeMeasures = result;
+    });
+
+    this.apollo.subscribe({
+      query: MEASURES_HOME_SUBSCRIPTION,
+      /*
+        accepts options like `errorPolicy` and `fetchPolicy`
+      */
+    }).pipe(map((result: any) => result.data.measuresHomeAdded)).subscribe((result: any) => {
+      this.homeMeasures = [...this.homeMeasures, result];
+    });
   }
+
+
 }
